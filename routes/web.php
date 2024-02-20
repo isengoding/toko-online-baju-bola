a6f302c51a1d79abede5b2e92161d9d3e8b0c5f5
Dharma <isengoding@gmail.com>
2024-02-21 06:43:27 +0700

    project setup

---
+++ routes/web.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImageController;
+use App\Http\Controllers\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+
+Route::get('/combo', function () {
+    return view('layouts.app');
+});
+Route::get('/guest', function () {
+    return view('layouts.guest');
+});
+
+// Route::get('/products', [ProductController::class, 'index']);
+// Route::get('/products', [ProductController::class, 'index']);
+
+Route::post('upload', [ImageController::class, 'upload'])->name('images.upload');
+Route::delete('revert', [ImageController::class, 'revert'])->name('images.revert');
+
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::resource('images', ImageController::class);
+
+    Route::get('/products/import/create', [ProductController::class, 'importCreate'])->name('products.import.create');
+    Route::post('/products/import', [ProductController::class, 'importStore'])->name('products.import.store');
+    Route::get('/products/excel', [ProductController::class, 'excel'])->name('products.excel');
+    Route::get('/products/pdf', [ProductController::class, 'pdf'])->name('products.pdf');
+    Route::resource('products', ProductController::class);
+});
+
+
+Route::prefix('prototype')->group(function () {
+    Route::get('homepage', function () {
+        return view('prototype.homepage');
+    });
+});
+
