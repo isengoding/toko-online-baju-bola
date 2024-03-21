e4d6ec27757fa0df0506c58935aa6715b27607d3
Dharma <isengoding@gmail.com>
2024-03-21 09:50:39 +0700

    fix app guest layout

--- resources/views/pages/user/address/index.blade.php
+++ resources/views/pages/user/address/index.blade.php
@@ -29,8 +29,7 @@
                             </svg>
                             Add Address
                         </a>
-                        <a href="{{ route('user.addresses.create') }}" class="btn btn-primary d-sm-none btn-icon"
-                            data-bs-toggle="modal" data-bs-target="#modal-report" aria-label="Create new report">
+                        <a href="{{ route('user.addresses.create') }}" class="btn btn-primary d-sm-none btn-icon">
                             <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
                                 viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
